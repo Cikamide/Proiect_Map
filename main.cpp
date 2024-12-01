9068a05cbdb81d2b8172c478f6a9fc7e14398822
AndreiT24 <cikamide665@gmail.com>
2024-12-01 13:00:03 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,36 @@
-#include<iostream>
+#include <iostream>
+#include <string>
 using namespace std;
+
+int Granite[100][100];
+int i=0,j=0;
+
+struct tari
+{
+  int index;
+  string nume;
+}A[100];
+
 int main()
 {
-    cout<<"Hello nibba"<<endl;
+    int nr_tari, nr_culori;
+    string tara;
+    cout<<"Da-ti un numar de tari:"<<endl;;
+    cin>>nr_tari;
+    for(int k=0;k<nr_tari;k++)
+    {
+        cout<<"Introduceti urmatoarea tara: "<<endl;
+        getline(cin, tara);
+        A[i].nume=tara;
+        A[i].index=k+1;
+        i++;
+    }
+
+    for(int k=0;k<i;k++)
+    {
+        cout<<"Nr: "<<A[k].index<<" Numele: "<<A[k].nume<<endl; 
+    }
+
     
     return 0;
 }
